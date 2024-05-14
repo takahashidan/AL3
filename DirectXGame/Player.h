efc90f822205aaf8c252ce6defe1e6edadaf574e
takahashidan <k023g1019@g.neec.ac.jp>
2024-05-14 12:50:09 +0900

    02_01_確認課題

---
+++ DirectXGame/Player.h
@@ -0,0 +1,22 @@
+#include"Model.h"
+#include"WorldTransform.h"
+
+class Player {
+
+public:
+	void Initialize(Model*model,uint32_t textureHandle,ViewProjection*viewProjection);
+
+	void Update();
+
+	void Draw();
+
+private:
+    //ワールド変換データ
+	WorldTransform worldTransform_;
+	//モデル
+	Model* model_ = nullptr;
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+
+	ViewProjection* viewProjection_ = nullptr;
+};
--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -4,16 +4,35 @@
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() { 
+	//3Dモデルの解放
+	delete model_;
+	//自キャラの解放
+	delete player_;
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("uvChecker.png");
+	//3Dモデルデータの生成
+	model_ = Model::Create();
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+	//自キャラの生成
+	player_ = new Player();
+	//自キャラの初期化
+	player_->Initialize(model_,textureHandle_,&viewProjection_);
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	//自キャラの更新
+	player_->Update();
+}
 
 void GameScene::Draw() {
 
@@ -41,6 +60,9 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	
+	// 自キャラの描画
+	player_->Draw();
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
